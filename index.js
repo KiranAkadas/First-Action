4eed0111e53033827f8eaed79c8b8c1075bbdf0d
KiranAkadas <akadask@gmail.com>
2020-08-30 23:31:47 +0530

    Again

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const github = require('@actions/github');
 //const base = core.getInput('base_brancg', { required: true });
 async function run() {
   try {
+    console.log("Ok so is it getting printed?")
     const context = github.context;
+    console.log("Ok so is it getting printed?")
     const pullRequest=context.payload.pull_request.head.ref;
+    console.log("Ok so is it getting printed?")
     const base=context.base_ref
+    console.log("Ok so is it getting printed?")
     await exec.exec('git diff --stat ',[base,pullRequest]);
     console.log("Ok so is it getting printed?")
     console.log(base,pullRequest)
