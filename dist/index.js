2bc741da084ef975ca87ce79e53795c47df674d7
KiranAkadas <akadask@gmail.com>
2020-08-31 00:33:44 +0530

    ok

--- dist/index.js
+++ dist/index.js
@@ -9,52 +9,21 @@ const core = __webpack_require__(186);
 const exec = __webpack_require__(514);
 const github = __webpack_require__(438);
 
-const src = __dirname;
-
 //const prb = core.getInput('PR_branch', { required: true });
 //const base = core.getInput('base_brancg', { required: true });
 async function run() {
   try {
-
+  
     const context = github.context;
     const pullRequest=context.payload.pull_request.head.ref;
     const base=context.payload.pull_request.base.ref;
-    await exec.exec("git log --oneline",["WebTechnologies_PasswordSecurity/master", "WebTechnologies_PasswordSecurity/test4"])
-    getCommitDifference(base, pullRequest);
-  
+    await exec.exec('git diff --stat ',["WebTechnologies_PasswordSecurity/master","WebTechnologies_PasswordSecurity/test4"]);
+    console.log("Ok so is it getting printed?")
+    //console.log(base,pullRequest)
   } catch (error) {
     core.setFailed(error.message);
   }
 }
-
-async function getCommitDifference(baseBranch, secondaryBranch) {
-  try {
-    let output = '';
-    let err = '';
-
-    // These are option configurations for the @actions/exec lib`
-    const options = {};
-    options.listeners = {
-      stdout: (data) => {
-        output += data.toString();
-      },
-      stderr: (data) => {
-        err += data.toString();
-      }
-    };
-    options.cwd = './';
-
-    await exec.exec(__webpack_require__.ab + "script.sh", [baseBranch, secondaryBranch], options);
-    const { commitDiffCount } = JSON.parse(output);
-
-    console.log('\x1b[32m%s\x1b[0m', `Difference in commits between ${secondaryBranch} and ${baseBranch}: ${commitDiffCount}`);
-  
-  } catch (err) {
-    core.setFailed(`Could not get commit difference between branches because: ${err.message}`);
-    process.exit(0);
-  }
-}
-
 run();
 
 
--- index.js
+++ index.js
@@ -2,50 +2,19 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 
-const src = __dirname;
-
 //const prb = core.getInput('PR_branch', { required: true });
 //const base = core.getInput('base_brancg', { required: true });
 async function run() {
   try {
-
+  
     const context = github.context;
     const pullRequest=context.payload.pull_request.head.ref;
     const base=context.payload.pull_request.base.ref;
-    await exec.exec("git log --oneline",["WebTechnologies_PasswordSecurity/master", "WebTechnologies_PasswordSecurity/test4"])
-    getCommitDifference(base, pullRequest);
-  
+    await exec.exec('git diff --stat ',["WebTechnologies_PasswordSecurity/master","WebTechnologies_PasswordSecurity/test4"]);
+    console.log("Ok so is it getting printed?")
+    //console.log(base,pullRequest)
   } catch (error) {
     core.setFailed(error.message);
   }
 }
-
-async function getCommitDifference(baseBranch, secondaryBranch) {
-  try {
-    let output = '';
-    let err = '';
-
-    // These are option configurations for the @actions/exec lib`
-    const options = {};
-    options.listeners = {
-      stdout: (data) => {
-        output += data.toString();
-      },
-      stderr: (data) => {
-        err += data.toString();
-      }
-    };
-    options.cwd = './';
-
-    await exec.exec(`${src}/script.sh`, [baseBranch, secondaryBranch], options);
-    const { commitDiffCount } = JSON.parse(output);
-
-    console.log('\x1b[32m%s\x1b[0m', `Difference in commits between ${secondaryBranch} and ${baseBranch}: ${commitDiffCount}`);
-  
-  } catch (err) {
-    core.setFailed(`Could not get commit difference between branches because: ${err.message}`);
-    process.exit(0);
-  }
-}
-
 run();
