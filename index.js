26ad946c52bbe26e3d9556205202cd4a7b72a2b8
KiranAkadas <akadask@gmail.com>
2020-08-30 23:09:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ const github = require('@actions/github');
 async function run() {
   try {
     //const myToken = core.getInput('myToken');
+    let myOutput = '';
+    let myError = '';
+
+    const options = {};
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        myError += data.toString();
+      }
+    };
+    options.cwd = './lib'
     const context = github.context;
     const pullRequest=context.payload.pull_request.head.ref;
     const base=context.base_ref
-    await exec.exec('git diff --stat ',[base,pullRequest]);
+    await exec.exec('git diff --stat ',[base,pullRequest],options);
+    console.log(options)
   } catch (error) {
     core.setFailed(error.message);
   }
