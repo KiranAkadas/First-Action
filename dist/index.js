554706f8d91b622410cc6e8f6620d2f616677e46
KiranAkadas <akadask@gmail.com>
2020-09-01 23:50:15 +0530

    Update

--- dist/index.js
+++ dist/index.js
@@ -17,6 +17,7 @@ async function run() {
     const context = github.context;
     const pullRequest=context.payload.pull_request.head.ref;
     const base=context.payload.pull_request.base.ref;
+    await exec.exec('git fetch origin/master')
     await exec.exec('git diff --stat ',["origin/master","origin/test4"]);
     console.log("Ok so is it getting printed?")
     //console.log(base,pullRequest)
