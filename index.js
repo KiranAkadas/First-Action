5ad3cd3321f4b9881bd266be66db0a7681f72c7a
KiranAkadas <akadask@gmail.com>
2020-09-02 00:29:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
+const wait = require('./wait');
 
 //const prb = core.getInput('PR_branch', { required: true });
 //const base = core.getInput('base_brancg', { required: true });
