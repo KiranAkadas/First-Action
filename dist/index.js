11377aca2114555ac346136b852a825da87f0226
KiranAkadas <akadask@gmail.com>
2020-09-06 15:14:15 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -16,9 +16,15 @@ async function run() {
   try {
     console.log("Ok so is it getting printed?")
     const context = github.context;
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken);
+
     const pullRequest=context.payload.pull_request.head.ref;
     const create=context.payload.pull_request.created_at;
     const update=context.payload.pull_request.updated_at;
+    const repo_create=context.payload.pull_request.head.repo.created_at;
+    const repo_update=context.payload.pull_request.head.repo.updated_at;
+    const repo_push=context.payload.pull_request.head.repo.pushed_at;
     const contri=context.payload.pull_request.head.repo.contributors_url
     // const repo=context.payload.pull_request.head.repo.html_url;
     // const reponame=context.payload.pull_request.head.repo.name;
@@ -28,6 +34,12 @@ async function run() {
     // await exec.exec('cd ',[reponame])
     //await exec.exec('git log --stat ',[pullRequest]);
     console.log(create,update,contri)
+    const newIssue = await octokit.issues.create({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: 'Some Stats :\n Created at:'
+    });
     //console.log(base,pullRequest)
   } catch (error) {
     core.setFailed(error.message);
